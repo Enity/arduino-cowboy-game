1711abe6c85b35822b16a43eef7bb865011cd6e1
vlad-boroda <enity.w@gmail.com>
2019-11-17 22:03:53 +0300

    theme

--- mario.cpp
+++ mario.cpp
@@ -163,22 +163,22 @@ void buzz(int targetPin, long frequency, long length) {
 }
 
 void sing(int soundPin) {
-    int size = sizeof(melody) / sizeof(int);
-    for (int thisNote = 0; thisNote < size; thisNote++) {
+  int size = sizeof(melody) / sizeof(int);
+  for (int thisNote = 0; thisNote < size; thisNote++) {
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-      int noteDuration = 1000 / tempo[thisNote];
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / tempo[thisNote];
 
-      buzz(soundPin, melody[thisNote], noteDuration);
+    buzz(soundPin, melody[thisNote], noteDuration);
 
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
 
-      // stop the tone playing:
-      buzz(soundPin, 0, noteDuration);
-    }
+    // stop the tone playing:
+    buzz(soundPin, 0, noteDuration);
+  }
 }
